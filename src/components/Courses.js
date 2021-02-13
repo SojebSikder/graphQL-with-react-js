44dab031467c471bf32a089c8752f5cee95daf2f
sojebsikder <sojebsikder@gmail.com>
2021-02-13 18:41:01 +0600

    main

--- src/components/Courses.js
+++ src/components/Courses.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import { Query } from "react-apollo";
+import gql from 'graphql-tag';
+
+// alt + num9 + num6 for `this`
+
+const userQuery = gql`
+  query GetDogs {
+    dogs {
+      id
+      breed
+    }
+  }
+`;
+
+
 
 export default function Courses() {
     return (
-        <div>
-            Hello World
-        </div>
-    )
+        <Query query={userQuery} />
+
+    );
 }
